3318559cd31169ae4e184b36cb0118ce9ca2e14f
MirulNasir <61763102+mirulnasir@users.noreply.github.com>
2024-07-02 07:48:45 +0800

    feat(theme setup): :lipstick: add design colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,15 @@ module.exports = {
       maxWidth: {
         '8xl': '1376px',
       },
+      colors: {
+        'brand-primary-blue': '#1C75BC',
+        'brand-secondary-off-white': '#EAF4F6',
+        'brand-secondary-light-blue': '#83B7E6',
+        'brand-secondary-blue': '#0F4C85',
+        'brand-secondary-dark-blue': '#00305B',
+        'brand-secondary-dark-gray': '#58595B',
+        'brand-primary-black': '#1C1B1B',
+      },
       dropShadow: {
         'heading': '0 4px 4px rgba(0, 0, 0, 0.25)',
       }
